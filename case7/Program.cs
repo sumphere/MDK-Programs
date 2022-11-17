a809ddf0cb3c2ebc9d0bfcde38f5e32b1b6b9fed
sumphere <116386070+sumphere@users.noreply.github.com>
2022-11-17 16:24:46 +0000

    case7, for27

---
+++ case7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace case7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите единицу массы");
+                var massType = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите массу");
+                var mass = double.Parse(Console.ReadLine());
+
+                double mul = 1;
+                var badMassType = false;
+                switch (massType)
+                {
+                    default:
+                        badMassType = true;
+                        break;
+                    case 1:
+                        break;
+                    case 2:
+                        mul = 1e-6;
+                        break;
+                    case 3:
+                        mul = 0.001;
+                        break;
+                    case 4:
+                        mul = 1000;
+                        break;
+                    case 5:
+                        mul = 100;
+                        break;
+                }
+
+                if (!badMassType) Console.WriteLine(mass * mul);
+                else Console.WriteLine("Неверный тип массы");
+            }
+        }
+    }
+}
---
+++ for27/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace for27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите X");
+                var x = double.Parse(Console.ReadLine());
+                if (Math.Abs(x) >= 1)
+                {
+                    Console.WriteLine("|X| должно быть меньше 1");
+                    continue;
+                }
+                Console.WriteLine("Введите N");
+                var n = int.Parse(Console.ReadLine());
+
+                double result = 0;
+                double pow = x;
+                double temp1 = 1;
+                double temp2 = 1;
+                for (int i = 1; i < n - 1; i++)
+                {
+                    temp1 *= 2 * i - 1;
+                    temp2 *= 2 * i;
+                    pow *= x * x;
+                    result += temp1 * pow / (temp2 * (2 * i + 1));
+                }
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
