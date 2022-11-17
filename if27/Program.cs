ecb1a0d33abde03ed9ed51537ba75ed304dca736
NieEonliv <63141394+NieEonliv@users.noreply.github.com>
2022-11-17 15:05:31 +0000

    if27

---
+++ if27/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace if27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите x");
+                var x = double.Parse(Console.ReadLine());
+                int f = 0;
+
+                if (x < 0) f = 0;
+                else if (x % 2 < 1)
+                    f = 1;
+                else if (x % 2 >= 1)
+                    f = -1;
+                Console.WriteLine(f);
+            }
+        }
+    }
+}
