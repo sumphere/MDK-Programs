6d7cf4e8a872794eccf86ca4fea88aef8f3c997d
sumphere <116386070+sumphere@users.noreply.github.com>
2022-11-22 18:27:47 +0300

    14.27

---
+++ 14.27/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _14._27
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("./data.txt");
+            Console.WriteLine(lines[0]);
+
+            Regex regex = new(@"\d+");
+            
+            Console.WriteLine($"a) {regex.Replace(lines[0], v => int.Parse(v.Value) % 2 == 0 ? "0" : v.Value, 13)}");
+            Console.WriteLine($"б) {regex.Replace(lines[0], v => int.Parse(v.Value) % 2 == 0 ? "0" : v.Value)}");
+        }
+    }
+}
