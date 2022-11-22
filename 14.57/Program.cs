2155fa502a2275559a25acca1fc26ce942e898e3
sumphere <116386070+sumphere@users.noreply.github.com>
2022-11-22 18:27:55 +0300

    14.57

---
+++ 14.57/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _14._57
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("./data.txt");
+            var symbols = lines[0].ToCharArray();
+            var numSymbols = "";
+            var otherSymbols = "";
+            foreach(var symbol in symbols)
+            {
+                if (int.TryParse(symbol.ToString(), out _))
+                    numSymbols += symbol;
+                else
+                    otherSymbols += symbol;
+            }
+            Console.WriteLine(numSymbols);
+            Console.WriteLine(otherSymbols);
+
+            File.WriteAllText("./numData.txt", numSymbols);
+            File.WriteAllText("./otherData.txt", otherSymbols);
+        }
+    }
+}
