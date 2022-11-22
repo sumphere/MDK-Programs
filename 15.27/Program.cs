34227097e6cb66cf1a9cbe12230fb384534da830
sumphere <116386070+sumphere@users.noreply.github.com>
2022-11-22 18:28:07 +0300

    15.27

---
+++ 15.27/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace _15._27
+{
+    internal class Program
+    {
+        static void Main(string[] args) =>
+            File.WriteAllLines("./data2.txt", File.ReadAllLines("./data.txt").Where(l => l.Length > 30));
+    }
+}
