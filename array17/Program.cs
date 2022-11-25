e8fe0e7fc557994f0a2e300066b3067947c752a0
sumphere <116386070+sumphere@users.noreply.github.com>
2022-11-25 12:51:16 +0300

    array17

---
+++ array17/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace array17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+            var n = arr.Length;
+            for (int i = 0; i < n / 2 + 1; i += 2)
+            {
+                if (i <= n / 2) Console.Write(arr[i] + " ");
+                if (i <= n / 2 - 1) Console.Write(arr[i + 1] + " ");
+                if (n - 1 - i > n / 2) Console.Write(arr[n - 2 - i + 1] + " ");
+                if (n - 1 - i > n / 2) Console.Write(arr[n - 2 - i] + " ");
+            }
+        }
+    }
+}
