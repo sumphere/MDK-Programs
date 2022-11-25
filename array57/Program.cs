93c5aca7c5607069284b05d2eda8bb0d4be996fb
sumphere <116386070+sumphere@users.noreply.github.com>
2022-11-25 13:25:27 +0300

    array87

--- array57/Program.cs
+++ array57/Program.cs
@@ -11,10 +11,12 @@ namespace array57
             var arr = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
             var n = arr.Length;
             var arr2 = new List<int>();
+
             for (int i = 0; i < n; i += 2)
                 arr2.Add(arr[i]);
             for (int i = 1; i < n; i+=2)
                 arr2.Add(arr[i]);
+
             foreach (var v in arr2)
                 Console.Write(v + " ");
             Console.WriteLine();
---
+++ array87/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace array87
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rand = new Random();
+            var n = 10;
+            var arr = new int[n - 1].Select(_ => rand.Next(100)).ToList();
+            arr.Sort();
+            arr = arr.Prepend(rand.Next(100)).ToList();
+            foreach (var item in arr)
+                Console.Write(item + " ");
+            Console.WriteLine();
+
+            arr.Sort();
+            foreach (var item in arr)
+                Console.Write(item + " ");
+            Console.WriteLine();
+        }
+    }
+}
