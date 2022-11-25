d76980551bf4ac48632131fc202b274bf96b50ee
sumphere <116386070+sumphere@users.noreply.github.com>
2022-11-25 10:57:36 +0300

    proc56, 57

---
+++ proc56/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace proc56
+{
+    public class Proc
+    {
+        public static double[] InputCords() => Console.ReadLine().Split(" ").Select(x => double.Parse(x)).ToArray();
+        public static double Leng(double xa, double ya, double xb, double yb)
+        => Math.Sqrt(Math.Pow(xa - xb, 2) + Math.Pow(ya - yb, 2));
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты точки A через пробел");
+            var a = Proc.InputCords();
+            Console.WriteLine("Введите координаты точки B через пробел");
+            var b = Proc.InputCords();
+            Console.WriteLine("Введите координаты точки C через пробел");
+            var c = Proc.InputCords();
+            Console.WriteLine("Введите координаты точки D через пробел");
+            var d = Proc.InputCords();
+
+            Console.WriteLine($"|AB| = {Proc.Leng(a[0], a[1], b[0], b[1])}");
+            Console.WriteLine($"|AC| = {Proc.Leng(a[0], a[1], c[0], c[1])}");
+            Console.WriteLine($"|AD| = {Proc.Leng(a[0], a[1], d[0], d[1])}");
+        }
+    }
+}
---
+++ proc57/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace proc57
+{
+    class Proc : proc56.Proc
+    {
+        public static double Perim(double xa, double ya, double xb, double yb, double xc, double yc)
+             => Leng(xa, ya, xb, yb) + Leng(xb, yb, xc, yc) + Leng(xc, yc, xa, ya);
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты точки A через пробел");
+            var a = Proc.InputCords();
+            Console.WriteLine("Введите координаты точки B через пробел");
+            var b = Proc.InputCords();
+            Console.WriteLine("Введите координаты точки C через пробел");
+            var c = Proc.InputCords();
+            Console.WriteLine("Введите координаты точки D через пробел");
+            var d = Proc.InputCords();
+
+            Console.WriteLine($"Периметр ABC = {Proc.Perim(a[0], a[1], b[0], b[1], c[0], c[1])}");
+            Console.WriteLine($"Периметр ABD = {Proc.Perim(a[0], a[1], b[0], b[1], d[0], d[1])}");
+            Console.WriteLine($"Периметр ACD = {Proc.Perim(a[0], a[1], c[0], c[1], d[0], d[1])}");
+        }
+    }
+}
