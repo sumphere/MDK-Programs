c2ce5b687cf558fbc03417fae9f7cbb6ba93de9a
sumphere <116386070+sumphere@users.noreply.github.com>
2022-11-25 13:11:43 +0300

    array57

---
+++ array57/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace array57
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+            var n = arr.Length;
+            var arr2 = new List<int>();
+            for (int i = 0; i < n; i += 2)
+                arr2.Add(arr[i]);
+            for (int i = 1; i < n; i+=2)
+                arr2.Add(arr[i]);
+            foreach (var v in arr2)
+                Console.Write(v + " ");
+            Console.WriteLine();
+        }
+    }
+}
