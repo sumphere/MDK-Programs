7c2626c348b25e12c8553219fb32b7ee7c867ab9
sumphere <116386070+sumphere@users.noreply.github.com>
2022-11-25 10:14:01 +0300

    proc27

---
+++ proc27/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace proc27
+{
+    class Program
+    {
+        static bool IsPowerN(double k, double n)
+        {
+            if (k < n) return false;
+            while (k % n == 0)
+                k /= n;
+            return k == 1;
+        }
+        static void Main(string[] args)
+        {
+            var array = new int[] { 1, -1, 5, 15, 25, 125, 625, 6, 10, 11 };
+            var n = 5;
+            var count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (IsPowerN(array[i], n)) count++;
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
