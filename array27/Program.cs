1303e9bafe77bacc2021080ed2cae2dec2fe48f8
sumphere <116386070+sumphere@users.noreply.github.com>
2022-11-25 13:01:49 +0300

    array27

---
+++ array27/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace array27
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arr = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+            var n = arr.Length;
+            foreach (var v in arr)
+                Console.Write(v + " ");
+            Console.WriteLine();
+
+            for (int i = 1; i < n; i++)
+            {
+                if (Math.Sign(arr[i - 1]) == Math.Sign(arr[i]))
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
+            Console.WriteLine(0);
+        }
+    }
+}
